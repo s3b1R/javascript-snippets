e5f70eebd81d79e8f61d6b56e62c59a8fc6be176
Ueli Kunz <kunz@ideadapt.net>
2017-03-27 13:31:46 +0200

    fibonacci exercise with solution

---
+++ src/exercises/fibonacci.js
@@ -0,0 +1,6 @@
+// fibonacci, imperativ
+
+// Berechne die Fibonacci Folge für n Elemente (Variabel, 0 - unendlich)
+// Gib alle berechneten Elemente aus
+
+
---
+++ src/exercises/solutions/fibonacci.sol.js
@@ -0,0 +1,10 @@
+// fibonacci, imperativ
+
+let n = 10
+let fib = [0,1]
+for(let i=2; fib.length<n; i++){
+    fib.push(fib[i-2]+fib[i-1])
+}
+fib = fib.splice(0, n)
+
+console.log(fib)
