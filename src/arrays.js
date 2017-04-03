2da8d8dd5e0e4e01d5435e9d1ff7e57414ce6fef
Ueli Kunz <kunz@ideadapt.net>
2017-04-03 20:49:32 +0200

    some more examples for operators

--- src/arrays.js
+++ src/arrays.js
@@ -16,11 +16,13 @@ persons.push(
 let anna = persons.pop()
 
 // return all persons that match filter
+// persons wird nicht geändert
 let startWithA = persons.filter(function(p){
     return p.name.startsWith('M')
 })
 
 // create new objects from each person
+// persons wird nicht geändert
 let withLowerCase = persons.map(function(p){
     return {
         name: p.name.toLowerCase()
@@ -28,4 +30,13 @@ let withLowerCase = persons.map(function(p){
 })
 
 // convert to string
-persons.map(p => p.name).join(', ')
+let csv = persons.map(p => p.name).join(', ')
+
+// entferne die ersten beiden elemente aus dem array
+persons.splice(0, 2)
+
+let numbers = [0, 1, 2, 3]
+// lösche ab index 2, ein element, füge ab index 2 ein element 23 ein.
+// => ersetze element an index 2 mit 23
+// verändert numbers
+numbers.splice(2, 1, 23)
---
+++ src/operators.js
@@ -0,0 +1,28 @@
+'use strict'
+// == vs ===
+
+// equality
+let a
+a = '0' == 0
+a = [] == ''
+a = [] == 0
+a = [0] == 0
+a = false == []
+a = false == 0
+a = new Boolean(false) ? 't' : 'f'
+a = new Boolean(false) == false
+
+// logical
+// evaluiere solange bis erster true => weisse den zu
+a = 0 || 200 || alert('hi')
+// evaluiere solange bis erster false ist => weisse den zu
+a = 200 && 0 && alert('hi')
+// teste ersten OR operand
+    // teste obj => truethy
+    // teste obj.opt1 truethy
+// erster operand true => brich ab
+// weisse wert von zuletzt evaluiertem operand (obj.opt1) zu
+let obj = { opt1: '3434' }
+let opt1Value = (obj && obj.opt1) || 'opt1DefaultValue'
+let tryIt = _=>false, fail = _=>'fail'
+a = tryIt() || fail()
